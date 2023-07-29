61609b19f6a6766c90d075f5ffdc8122599ea772
Prashant Singh <60805169+prashant2599@users.noreply.github.com>
2023-07-29 11:56:52 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,22 @@
+variable "ec2_region" {
+    default = "ap-south-1"
+}
+
+variable "ec2_name" {
+    default = "tier arch"
+}
+
+variable "ec2_instance_type" {
+    default = "t2.micro"
+}
+
+variable "ec2_image" {
+    default = "ami-008b85aa3ff5c1b02"
+}
+
+
+
+
+
+
+
